4d6265e74658bdda7b47e4a98293bf502dee624d
Chaikin Evgenii <fat_hack@rambler.ru>
2017-05-04 18:09:51 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+class TW
+{
+    // Set minimum length of array
+    protected $amountMin = 2;
+
+    // Result
+    protected $result = -1;
+
+    // Data
+    protected $data = [];
+
+    /**
+     * TW constructor.
+     * @param int $length
+     */
+    public function __construct($length = 0)
+    {
+        // Set to min length of raary
+        if ($length < $this->amountMin) {
+            $length = $this->amountMin;
+        }
+
+        // Create array range
+        $this->data = range(1, $length);
+
+        // Shuffle created array
+        shuffle($this->data);
+
+        $minPos = 0;
+        for ($i = 0; $i < $length; $i++) {
+            if ($this->data[$i] == 1) {
+                $minPos = $i;
+                break;
+            }
+        }
+
+        for ($i = 0; $i < $length - 1; $i++) {
+            if ($i + 1 < $minPos) {
+                if ($this->data[$i] > $this->data[$i + 1]) {
+                    return null;
+                }
+            } elseif ($i + 1 == $minPos) {
+                if ($this->data[$i] < $this->data[$i + 1]) {
+                    return null;
+                }
+            } elseif ($i + 1 >= $minPos) {
+                if ($this->data[$i] + 1 != $this->data[$i + 1]) {
+                    return null;
+                }
+            } elseif ($i == 0 && $i == $minPos) {
+                if ($this->data[$i] + 1 != $this->data[$i + 1]) {
+                    return null;
+                }
+            }
+        }
+
+        $this->result = $minPos > 0 ? $length - $minPos : 0;
+
+        return null;
+    }
+
+    /**
+     * Return generated array
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        return print_r($this->data, true);
+    }
+
+    /**
+     * Return result
+     *
+     * @return int
+     */
+    public function getResult()
+    {
+        return $this->result;
+    }
+}
+
+// Parameter sets the length of array
+$instance = new TW(3);
+echo '<br />Data: ' . $instance->getData();
+echo '<br />Result: ' . $instance->getResult();
